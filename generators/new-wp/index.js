31376070baff47bcfb38e27a2d8b06c1b32fecda
adam <afenton@getmoxied.net>
2016-04-19 17:47:24 -0500

    #5 Set-up git

--- generators/new-wp/index.js
+++ generators/new-wp/index.js
@@ -1,21 +1,7 @@
 var generators = require('yeoman-generator');
 
-var defaultValue = function() {
-  return '';
-}
-
-var createKeywords = function(str) {
-  return str
-    .split(',')
-    .map(function(s) {
-      return s.trim();
-    })
-    .filter(function(s) {
-      return s && s.length > 0;
-    });
-};
-
 module.exports = generators.Base.extend({
+
   prompts: function() {
     var done = this.async();
 
@@ -23,14 +9,26 @@ module.exports = generators.Base.extend({
       {
         name: 'name',
         message: 'The name of this project',
-        default: defaultValue
+        default: this.destinationRoot().split('/').pop(),
+        validate: function( input ) {
+          return input.trim() !== '';
+        }
+      },
+      {
+        name: 'repo',
+        message: 'The project repo uri (set it up in GitHub/Bitbucket first!)',
+        validate: function( input ) {
+          return input.indexOf('github.com') > -1 || input.indexOf('bitbucket.org') > -1;
+        }
       }
     ];
     this.prompt(questions, function(answers) {
       this.name = answers.name.toLowerCase().trim().replace(/\s/g, '-');
+      this.repo = answers.repo;
       done();
     }.bind(this));
   },
+
   writing: function() {
     this.fs.copyTpl(
       this.templatePath('composer.json'),
@@ -55,5 +53,15 @@ module.exports = generators.Base.extend({
         name: this.name
       }
     );
+  },
+
+  install: function() {
+    this.spawnCommandSync('git', ['init']);
+    this.spawnCommandSync('git', ['add', '--all']);
+    this.spawnCommandSync('git', ['commit', '-m', 'Initial commit', '--quiet']);
+    this.spawnCommandSync('git', ['remote', 'add', 'origin', this.repo]);
+    this.spawnCommandSync('git', ['push', '-u', 'origin', 'master']);
+    this.spawnCommandSync('git', ['checkout', '-b', 'develop']);
+    this.spawnCommandSync('git', ['push', '--set-upstream', 'origin', 'develop']);
   }
 });
