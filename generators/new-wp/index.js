663d1bbc98afc1e23e42853faacd0cab32f46667
adam <afenton@getmoxied.net>
2016-04-27 21:57:07 -0500

    #19 Only promt for project name once in new-wp

--- generators/new-wp/index.js
+++ generators/new-wp/index.js
@@ -1,10 +1,33 @@
+require('../../shared/string');
+var shared = require('../../shared/functions');
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
 
-  initializing: function () {
-    this.composeWith('moxie-lean:new-wp-project');
-    this.composeWith('moxie-lean:new-wp-plugin');
-  }
+  prompts: function() {
+    var questions = [
+      {
+        name: 'name',
+        message: 'The name of the project',
+        default: shared.getBasename( this.destinationRoot() ),
+        validate: function( input ) {
+          return !input.isEmpty();
+        }
+      }
+    ];
 
+    this.prompt(questions, function(answers) {
+      this.name = answers.name.cleanProjectName();
+      this._compose();
+    }.bind(this));
+  },
+
+  _compose: function () {
+    var options = { options: {
+      name: this.name
+    }};
+
+    this.composeWith('moxie-lean:new-wp-project', options);
+    this.composeWith('moxie-lean:new-wp-plugin', options);
+  }
 });
